186c030d906e76fc10b23fcb98effc78c7b8e0a6
trongthuong96 <trongthuong96@outlook.com.vn>
2022-12-05 22:32:34 +0700

    add post list chapter (chapter controller)

--- API/Controllers/ChapterController.cs
+++ API/Controllers/ChapterController.cs
@@ -129,6 +129,63 @@ namespace API.Controllers
             return CreatedAtRoute("GetChapter", new { chapterId = chapterObj.Id, storyId = chapterObj.StoryId}, chapterObj);
         }
 
+        //post list chap
+        [HttpPost("list-chap")]
+        [ProducesResponseType(201, Type = typeof(ChapterDto))]
+        //[ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateChapter([FromBody] IList<ChapterCreateDto> chapterCreateDtoList)
+        {
+
+            // check info null
+            if (chapterCreateDtoList == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            foreach(ChapterCreateDto chapterCreateDto in chapterCreateDtoList)
+            {
+                // check story
+                if (!_storyRepository.StoryExists(chapterCreateDto.StoryId)) //true
+                {
+                    return NotFound("Truyện này chưa được tạo!");
+                }
+
+                // check name chapter -- unique
+                if (_chapterRepository.ChapterExists(chapterCreateDto.Name, chapterCreateDto.StoryId))
+                {
+                    ModelState.AddModelError("", "Đã tồn tại chương này!");
+                    return StatusCode(404, ModelState);
+                }
+
+                // check model
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                var chapterObj = new Chapter();
+
+                chapterObj = _mapper.Map<Chapter>(chapterCreateDto);
+
+                //add date
+                chapterObj.CreateDate = DateTime.Now;
+                chapterObj.UpdateDate = DateTime.Now;
+                chapterObj.Id = Guid.NewGuid();
+
+
+                // create chapter
+                if (!_chapterRepository.CreateChapter(chapterObj))
+                {
+                    ModelState.AddModelError("", $"Đã xảy ra sự cố khi lưu {chapterObj.Name}");
+                    return StatusCode(500, ModelState);
+                }
+            }
+            return Ok();
+        }
+
         [HttpPatch("{storyId:Guid}/{chapterId:Guid}", Name = "UpdateChapter")]
         [ProducesResponseType(200, Type = typeof(ChapterDto))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
